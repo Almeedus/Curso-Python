2813c76f9af63cd87f55e83a2559b515b9cc5e11
Eduardo Almeida <almeedusa@gmail.com>
2024-08-31 17:46:15 -0300

    Initializing the list outside the loop, defining the logic for adding and viewing tasks

--- Modulo 1, 2, 3 e 4/exercicio/lista_tarefas.py
+++ Modulo 1, 2, 3 e 4/exercicio/lista_tarefas.py
@@ -1,38 +1,57 @@
 from os import system
-# Lista de tarefas: Fazer e desfazer ações
 
-# todo = [] -> lista de tarefas
-# todo = ['fazer café'] -> Adicionar fazer café
-# todo = ['fazer café', 'Caminhar'] -> Adicionar fazer caminhar
 def add_task(task=str, todo_list=None):
     if todo_list is None:
         todo_list = []
-        
     todo_list.append(task)
     return todo_list
 
-# desfazer = ['fazer café'] -> Refazer ['caminhar']
-# desfazer = [] -> Refazer ['caminhar', 'fazer café']
-
-# refazer = todo ['fazer café']
-# refazer = todo ['fazer café', 'caminhar']
-
-
+list_task = []
 while True:
+    option_list = ["Add task", "Undo task", "Redo task", "Visualize List"]
+    
     print("To-do list:")
-    print("Add task - Undo task - Redo task")
+    for _index, option in enumerate(option_list):
+        print("[{}] - {}".format(_index+1, option))
+
+    print("Choose an option.")
     
     try:
-        user_choose = input("USER-TYPE: ")
-        user_choose = str(user_choose).strip().capitalize()
+        user_choose = input()
+        user_choose = int(user_choose)-1
         
     except ValueError:
         _red = ["\033[31m","\033[m"]
-        print(_red[0],"Wrong value! Type only text.",_red[1])
+        print(_red[0],"Wrong value! Type only numbers.",_red[1])
         input("Type enter to reload.")
         system("clear")
         continue
     
+    if user_choose == 0:
+        # adicionar = [] -> lista de tarefas
+        # adicionar = ['fazer café'] -> Adicionar fazer café
+        system("clear")
+        task_user = str(input("Task name: "))
+        add_task(task=task_user, todo_list=list_task)
+        print("")
+        
+    elif user_choose == 1:
+        # desfazer = ['fazer café'] -> Refazer ['caminhar']
+        # desfazer = [] -> Refazer ['caminhar', 'fazer café']
+        system("clear")
+        input("enter under task")
+        
+    elif user_choose == 2:
+        # refazer = todo ['fazer café']
+        # refazer = todo ['fazer café', 'caminhar']
+        system("clear")
+        input("enter redo task")
     
-    
+    elif user_choose == 3:
+        # visualizar todo lista
+        system("clear")
+        print("TO DO LIST")
+        for _index, task in enumerate(list_task):
+            print("{}. {}".format(_index+1,task))
+        print("")
     
