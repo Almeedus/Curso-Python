5033ebbf39334ea2190e492f73847124aa82696c
Eduardo Almeida <almeedusa@gmail.com>
2024-08-31 16:43:17 -0300

    Creating menu option input

--- Modulo 1, 2, 3 e 4/exercicio/lista_tarefas.py
+++ Modulo 1, 2, 3 e 4/exercicio/lista_tarefas.py
@@ -1,3 +1,4 @@
+from os import system
 # Lista de tarefas: Fazer e desfazer ações
 
 # todo = [] -> lista de tarefas
@@ -16,3 +17,22 @@ def add_task(task=str, todo_list=None):
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
 
+
+while True:
+    print("To-do list:")
+    print("Add task - Undo task - Redo task")
+    
+    try:
+        user_choose = input("USER-TYPE: ")
+        user_choose = str(user_choose).strip().capitalize()
+        
+    except ValueError:
+        _red = ["\033[31m","\033[m"]
+        print(_red[0],"Wrong value! Type only text.",_red[1])
+        input("Type enter to reload.")
+        system("clear")
+        continue
+    
+    
+    
+    
