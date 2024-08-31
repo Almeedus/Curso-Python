3045f1aa12e70063b4bc983d156463e78356c165
Eduardo Almeida <almeedusa@gmail.com>
2024-08-30 21:57:11 -0300

    Initial exercise file

--- Modulo 1, 2, 3 e 4/exercicio/lista_tarefas.py
+++ Modulo 1, 2, 3 e 4/exercicio/lista_tarefas.py
@@ -1,9 +1,5 @@
 # Lista de tarefas: Fazer e desfazer ações
 
-# Música para codar =)
-# Everybody wants to rule the world - Tears for fears
-
-
 # todo = [] -> lista de tarefas
 # todo = ['fazer café'] -> Adicionar fazer café
 # todo = ['fazer café', 'Caminhar'] -> Adicionar fazer caminhar
