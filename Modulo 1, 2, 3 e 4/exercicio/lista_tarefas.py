79e4dfd3ee20fbe1dca01710e3d953994ff84f42
Eduardo Almeida <almeedusa@gmail.com>
2024-08-30 22:43:35 -0300

    Creating a function that add tasks to list

--- Modulo 1, 2, 3 e 4/exercicio/lista_tarefas.py
+++ Modulo 1, 2, 3 e 4/exercicio/lista_tarefas.py
@@ -3,9 +3,16 @@
 # todo = [] -> lista de tarefas
 # todo = ['fazer café'] -> Adicionar fazer café
 # todo = ['fazer café', 'Caminhar'] -> Adicionar fazer caminhar
+def add_task(task=str, todo_list=None):
+    if todo_list is None:
+        todo_list = []
+        
+    todo_list.append(task)
+    return todo_list
 
 # desfazer = ['fazer café'] -> Refazer ['caminhar']
 # desfazer = [] -> Refazer ['caminhar', 'fazer café']
 
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
+
