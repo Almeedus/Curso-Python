105ff5cc708bfe358ceb33991754fe1880c82e85
Eduardo Almeida <almeedusa@gmail.com>
2025-04-24 11:00:41 -0300

    Exemplo completo de atributo e métodos

--- Modulo 5/class.py
+++ Modulo 5/class.py
@@ -1,18 +1,13 @@
-class Pessoa:
-    def __init__(self, nome, idade, cpf):
-        self.nome = nome
-        self.idade = idade
-        self.cpf = cpf
+class Carro:
+    def __init__(self, marca, modelo, cor, ano, tipo):
+        self.marca = marca
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+        self.tipo = tipo
         
-    def saudacao(self):
-        print(f'Olá, meu nome é {self.nome} e tenho {self.idade} anos.')
-    
-p1 = Pessoa(nome='Lucas', idade=20, cpf='123.456.789-00')
-p2 = Pessoa(nome='Ana', idade=25, cpf='987.654.321-00')
-
-
-print(f'Nome: {p1.nome}, Idade: {p1.idade}, CPF: {p1.cpf}')
-p1.saudacao()
-print()
-print(f'Nome: {p2.nome}, Idade: {p2.idade}, CPF: {p2.cpf}')
-p2.saudacao()
+    def acelerar(self):
+        print(f'O carro {self.modelo} está acelerando.')
+        
+fusca = Carro('Volkswagen', 'Fusca', 'azul', 1970, 'gasolina')
+fusca.acelerar()
