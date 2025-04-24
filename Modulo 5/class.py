c0e9719680555219e1c0fb7d77b99a115f54710b
Eduardo Almeida <almeedusa@gmail.com>
2025-04-24 10:48:47 -0300

    Exemplo de mátodos

--- Modulo 5/class.py
+++ Modulo 5/class.py
@@ -4,12 +4,15 @@ class Pessoa:
         self.idade = idade
         self.cpf = cpf
         
+    def saudacao(self):
+        print(f'Olá, meu nome é {self.nome} e tenho {self.idade} anos.')
     
 p1 = Pessoa(nome='Lucas', idade=20, cpf='123.456.789-00')
 p2 = Pessoa(nome='Ana', idade=25, cpf='987.654.321-00')
 
 
 print(f'Nome: {p1.nome}, Idade: {p1.idade}, CPF: {p1.cpf}')
-
+p1.saudacao()
 print()
 print(f'Nome: {p2.nome}, Idade: {p2.idade}, CPF: {p2.cpf}')
+p2.saudacao()
