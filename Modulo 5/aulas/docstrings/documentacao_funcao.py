b2c2239707eb0333b24b8ac23fa5618caa31d61b
Eduardo Almeida <almeedusa@gmail.com>
2025-10-13 16:46:16 -0300

    Docstring com funções

---
+++ Modulo 5/aulas/docstrings/documentacao_funcao.py
@@ -0,0 +1,25 @@
+"""
+Módulo exemplo SOMA
+
+Este é o meu exemplo de documentação da função soma.
+"""
+
+variavel_1 = 1
+
+def soma(x: int | float, y: int | float) -> int | float:
+    """
+    Soma x e y
+    
+    :param x: Número 1
+    :type x: int or float
+    :param y: Número 2
+    :type: int or float
+    
+    :return: A soma entre x e y
+    :rtype: int or float
+    """
+
+variavel_2 = 2
+variavel_3 = 3
+variavel_4 = 4
+
