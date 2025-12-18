85e019c85b26c45c05a3ff975d5f1ee2006092e0
Eduardo Almeida <almeedusa@gmail.com>
2025-12-18 15:36:26 -0300

    Descrição de resolução de exercício

---
+++ Modulo 5/exercicios/sistema_banco.py
@@ -0,0 +1,16 @@
+link = 'https://github.com/Almeedus/Sistema-Bancario'
+
+detalhes = """
+A estrutura de código e algoritmo foi desenvolvida de modo mais profissional,
+optei assim por desacoplar o exercício do curso principal e criei um repositório 
+remoto exclusivo para ele.
+
+No repositório você irá encontrar:
+1. Estrutura de arquivos organizadas
+2. Lógica separada em módulos
+3. Lógicas de verificação e de processos específicos
+4. README explicativo e claro
+5. Mensagens de commits com os passos de desenvolvimento 
+"""
+
+print(f'{detalhes} \n| {link}')
